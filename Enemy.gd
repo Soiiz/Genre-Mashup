41f900981600acc2d1ef7c83bd4cf80278ce34fa
Sriharsha-Madala <madalasriharsha@gmail.com>
2022-10-10 22:05:21 -0700

    organized code for enemy, changed collision shape for player

--- Enemy.gd
+++ Enemy.gd
@@ -3,39 +3,50 @@ extends KinematicBody2D
 
 var player = null
 var velocity = Vector2.ZERO
-var speed = 100
+var MAX_SPEED = 40
+var ACCELERATION = 10
+onready var sprite = $Sprite
+const GRAVITY = 200.0
 
-#const GRAVITY = 32
-#const UP = Vector2.UP
+enum {
+	IDLE,
+	ATTACK,
+	CHASE
+}
+var state = IDLE
 
 func _physics_process(delta):
-	velocity = Vector2.ZERO
-	
-	if player != null:
-		if player.position.x < self.position.x:
-			velocity.x = -35
-		elif player.position.x > self.position.x:
-			velocity.x = 35
-		#velocity = position.direction_to(player.position) * speed
-		#velocity.y = 0
-		
-	else:
-		velocity = Vector2.ZERO
-		
-	#velocity = velocity.normalized()
-	#velocity = move_and_collide(velocity)
+	match state:
+		IDLE:
+			velocity = velocity.move_toward(Vector2.ZERO, 15 * delta)
+			seek_player()
+		ATTACK:
+			pass
+		CHASE:
+			if player != null:
+				var direction = (player.global_position - global_position)
+				direction.y = 0
+				direction.normalized()
+				velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
+			else:
+				state = IDLE
+			sprite.flip_h = velocity.x < 0 # flips sprite depending on velocity which depends on player location
+	velocity.y += delta * GRAVITY
 	velocity = move_and_slide(velocity)
+
+func can_see_player():
+	return player != null
 	
-func _on_Area2D_body_entered(body):
-	if body != self:
-		player = body
-		
-
-
+func seek_player():
+	if can_see_player():
+		state = CHASE
+	
 func _on_Timer_timeout():
 	pass # Replace with function body.
 	
-#func move():
-#	velocity.y += GRAVITY
-#	velocity.x = -speed
-	#velocity = move_and_slide(velocity, UP)
+func _on_PlayerDetectionZone_body_entered(body):
+	#if body != self:
+	player = body
+	
+func _on_PlayerDetectionZone_body_exited(body):
+	player = null
